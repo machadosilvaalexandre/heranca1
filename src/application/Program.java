17144023b544094e6f897a513fa3e74ef2de1dbc
Alexandre Machado <machado_silva@hotmail.com>
2020-07-05 15:33:30 -0400

    downcasting

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,6 +17,23 @@ public class Program {
 		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
 		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
 		
+		// DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.00);
+		
+		// BusinessAccount acc5 = (BusinessAccount)acc3; não fazer
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+    		acc5.loan(200.00);
+			System.out.println("Loan!");
+		}
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 
 	}
 
