7fc020dcbe274fe9b0c18f764079ac3bca8037fd
Alexandre Machado <machado_silva@hotmail.com>
2020-07-05 08:30:37 -0400

    Acresenctado classe SavingsAccount

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import entities.BusinessAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
