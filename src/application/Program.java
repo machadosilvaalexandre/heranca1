16e392b5f6e4350612ebb83bea801735e2937d7a
Alexandre Machado <machado_silva@hotmail.com>
2020-07-06 05:35:22 -0400

    polimorfismo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,22 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.00);
-		System.out.println(acc1.getBalance());
+		Account x = new Account(1020,"Alex",1000.00);
+		Account y = new SavingsAccount(1023, "Maria", 1000.00, 0.01);
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.00, 0.01);
-		acc2.withdraw(200.00);
-		System.out.println(acc2.getBalance());
+		x.withdraw(50.00);
+		y.withdraw(50.00);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 		
 		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.00, 500.00);
-		acc3.withdraw(200.00);
-		System.out.println(acc3.getBalance());
 	}
 
 }
